fbb04ca339cae98275d854d451eb72779e53f9d3
sos9174 <sos9174@gmail.com>
2020-10-15 16:16:19 +0800

    springboot_security

---
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import java.net.PasswordAuthentication;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.inMemoryAuthentication().withUser("admin").password("123456").roles("ADMIN");
+			
+	}
+	
+	 @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+	 }
+	 
+	 @Bean
+	 PasswordEncoder passwordEncoder() {
+		 return new BCryptPasswordEncoder();
+	 }
+
+}
